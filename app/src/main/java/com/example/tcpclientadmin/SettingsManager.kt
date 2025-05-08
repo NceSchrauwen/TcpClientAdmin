61b0ce253e53a6012ee3b9b36a30386e813fda1c
N. Schrauwen <schrauwen.nina@gmail.com>
2025-05-08 17:20:34 +0200

    updated the way the ip address is retreived and stored

--- app/src/main/java/com/example/tcpclientadmin/SettingsManager.kt
+++ app/src/main/java/com/example/tcpclientadmin/SettingsManager.kt
@@ -5,18 +5,12 @@ import org.json.JSONObject
 import java.io.File
 
 object SettingsManager {
-    private const val SETTINGS_FILE = "settings.json"
 
     fun getIp(context: Context): String {
-        val file = File(context.filesDir, SETTINGS_FILE)
+        val jsonFile = context.assets.open("settings.json").bufferedReader().use { it.readText() }
+        val ip = JSONObject(jsonFile).getString("ip")
 
-        // First-time setup with dummy IP
-        if (!file.exists()) {
-            file.writeText("""{"ip": "192.168.2.30"}""")
-        }
-
-        val json = JSONObject(file.readText())
-        return json.getString("ip")
+        return ip
     }
 
 }
